ea578835a131156e1a3ae71943abd7266f2e0b27
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-30 23:25:54 +0100

    Updated native core packages

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.MovieRateMobile',
+  id: 'org.nativescript.MovieRaterMobile',
   appResourcesPath: 'App_Resources',
   android: {
     v8Flags: '--expose_gc',
