1700b1975fcbb5bbd242f46861923045fa0b9e3a
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-31 14:38:11 +0100

    styling the mobile version

--- src/app/challenges/current-challenges/current-challenges.component.ts
+++ src/app/challenges/current-challenges/current-challenges.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core'
 })
 
 export class CurrentChallengeComponent {
-    challengeDescription = 'Demo text'
+    challengeDescription = ''
     currentChallenge = ''
 
     onSetChallenge(){
