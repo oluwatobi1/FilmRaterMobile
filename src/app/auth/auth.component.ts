ff947e4340bfd62af290b309608c723ad1bfb9ee
oluwatobi1 <akintunlesetobi@gmail.com>
2020-12-31 17:04:40 +0100

    setting up web auth on native

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,67 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { ApiService } from '../api.service';
+
+interface TokenObj{
+  token:string
+}
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css'],
+  moduleId:module.id
+})
+export class AuthComponent implements OnInit {
+
+
+  authForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl('')
+  })
+  registerMode = false;
+
+
+
+
+  constructor(
+    private apiService:ApiService,
+    private cookieService:CookieService,
+    private router:Router
+  ) { }
+
+  ngOnInit() {
+    const token = this.cookieService.get('mr-token')
+    if (token){
+      this.router.navigate(["movies/"])
+    }
+  }
+
+
+  saveForm(){
+    if (!this.registerMode){
+      this.loginUser()
+    }
+    else{
+      this.apiService.registerUser(this.authForm.value).subscribe(
+        result=>{
+          // Login user after register
+          this.loginUser()
+        },
+        error=>console.log(error),
+      )
+    }
+  }
+
+
+  loginUser(){
+    this.apiService.loginUser(this.authForm.value).subscribe(
+      (result:TokenObj)=>{this.cookieService.set("mr-token", result.token)
+      this.router.navigate(["movies/"])},
+      error=>console.log(error)
+    )
+  }
+
+}
---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthComponent } from './auth.component';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+const routes:Routes=[
+  {path:'auth', component:AuthComponent}
+]
+
+
+@NgModule({
+  declarations: [AuthComponent],
+  imports: [
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  exports:[RouterModule]
+})
+export class AuthModule { }
